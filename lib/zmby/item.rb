c5dd241fbdcbed361172b2f85e91c6e024eea406
Diego Escalante <diego.escalante01@gmail.com>
2015-03-30 23:14:54 -0600

    Add stack cap for Item

--- lib/zmby/item.rb
+++ lib/zmby/item.rb
@@ -1,18 +1,22 @@
 module Zmby
 	class Item
-		attr_reader :name, :count
-		def initialize(itemName, initialCount=1)
+		attr_reader :name, :count, :maxAmount
+		def initialize(itemName, initialCount=1, maxAmount=1)
 			@name = itemName
 			@count = initialCount
+			@maxAmount = maxAmount
 		end
 
 		def addAmount(amount)
 			@count += amount
+			if @count > @maxAmount 
+				@count = @maxAmount
+			end
 		end
 
 		def removeAmount(amount)
 			@count -= amount
-			if(@count < 0) 
+			if @count < 0 
 				@count = 0
 			end
 		end
