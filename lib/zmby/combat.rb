1f17d3a6ad5b3a485b513b0e5eba59324f45ead9
alro5921 <thornrose.alex@gmail.com>
2015-04-27 22:36:54 -0600

    little flavor

--- lib/zmby/combat.rb
+++ lib/zmby/combat.rb
@@ -3,7 +3,7 @@ require_relative 'enemy'
 class Combat
   def self.fight(current_player)
     enemy = Enemy.new
-
+    puts "An average zombie hears your footsteps! It attacks!"
     while enemy.health > 0
       puts "Your Health: #{current_player.health} -  Enemy Health: #{enemy.health}"
     
